e2803d7b3994bf424e6915340ca16acac9c78f7f
Denis <lithiumdenis@gmail.com>
2017-04-15 20:17:57 +0300

    First look

---
+++ AngularApp/SPA/Controllers/MainController.js
@@ -0,0 +1,16 @@
+﻿var MainController = function ($scope) {
+    $scope.models = {
+        locations: [
+            { id: "1", Location: "Воронеж" },
+            { id: "2", Location: "Москва" },
+            { id: "3", Location: "Санкт-Петербург" },
+        ]
+    };
+    $scope.selectedLocation = $scope.models.locations[0];
+
+    $scope.changeLocation = function (loc) {
+        $scope.selectedLocation = loc;
+    }
+}
+
+MainController.$inject = ['$scope'];
